5f20fadc5811b34bf060c6078faf9762487bba90
jesen jipo <jesen@JESEN.local>
2025-06-18 18:00:46 -0500

    new commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const app = express();
 app.get('/index', (req, res) =>
-    res.send('Hola desde el micro de Jesus JImenez argocd!'));
+    res.send('Hola desde el micro de Jesus Jimenez Diplomado Usabana argocd!'));
 app.listen(3000);
